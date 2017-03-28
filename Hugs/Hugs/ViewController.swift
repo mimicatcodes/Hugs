148764cf2c2922228fc92ca4760c899e8d39a016
Luna An <mimicatcodes@gmail.com>
2017-03-28 00:05:02 -0400

    Xcode file added

---
+++ Hugs/Hugs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hugs
+//
+//  Created by Mirim An on 3/28/17.
+//  Copyright © 2017 Mimicatcodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
